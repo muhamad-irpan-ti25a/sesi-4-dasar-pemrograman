f743e533a83bfe5dcde0f5c48da11e2fcb00b482
muhamadirpan266 <fotopribadi56@gmail.com>
2026-04-10 08:17:13 +0700

    innitial commit

---
+++ biodata.py
@@ -0,0 +1,15 @@
+f_name = input("masukkan nama lengkap : ")
+tempat_lahir = input("masukkan tempat lahir : ")
+tgl_lahir = int(input("masukkan tanggal lahir :"))
+umur = int(input("masukkan umur :"))
+alamat = input("masukkan alamat :")
+
+
+
+print("==========BIODATA SEDERHANA==========")
+print("nama : {}".format(f_name))
+print("tempat lahir {}".format(tempat_lahir))
+print("tanggal Lahir : {}".format(tgl_lahir))
+print("umur : {}".format(umur))
+print("alamat : {}".format(alamat))
+print("=====================================")
